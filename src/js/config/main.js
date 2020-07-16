72d54a4552e8bf668ff59c4b1b3e0dcaded5ee09
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2020-07-16 21:25:38 +0300

    npm audit fix

--- src/js/config/main.js
+++ src/js/config/main.js
@@ -2,6 +2,7 @@ export const NEWS_API_KEY = '11ca8d99d5cf4166b1d57ca37f73a68f';
 export const NEWS_PERIOD = 7; // За сколько дней грузить новости
 export const NEWS_LAZY_LOAD = 3; // Количество подгружаемых новостей
 export const URL_NOT_FOUND_IMAGE = './images/not-found.jpg';
+
 // export const MAIN_API_BASE_URL = NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://localhost:3000';
 
 // L export const MAIN_API_BASE_URL = 'http://localhost:3000';
